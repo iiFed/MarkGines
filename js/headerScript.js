e01bafeea1bd7baaa201846f91a39c5a87e9702c
iiFed <57118368+iiFed@users.noreply.github.com>
2019-12-26 19:04:41 -0700

    Add files via upload

---
+++ js/headerScript.js
@@ -0,0 +1,10 @@
+document.write(
+"<aside class='topNav nav-blue' id='topNav'>" +
+		"<div class='container navBar'>" +
+			"<a href='http://markgines.com'><h2>Home</h2> </a>" +
+			"<a href='mailto:mark@markgines.com?Subject=Hi%20Mark'><h2>E-Mail</h2> </a>" +
+			"<a href='http://markgines.com/files/resume.pdf'><h2>Resume</h2> </a>" +
+
+		"</div>" +
+	"</aside>"
+);
